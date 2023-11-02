189521f0b1ecd048b58aaeb2bf97564950e51f1d
Marthius Redler Gomes Passos <72816054+guridolofi@users.noreply.github.com>
2023-11-02 02:12:22 -0300

    interface update

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from modules import encounter
+from modules import encounter, navigation, journey_points, camping
+from data.acess_water_and_food import get_water_result, get_forage_result, get_hunt_result
 
-count = 20
 
-while True:
-    test = encounter.test_encounter('colina','normal','beginner', 'extreme')
-    if count > 0:
-        print(test)
-        print('')
-        count = count-1
-    else:
-        break
+
+print(journey_points.get_action_result('montanha', 'viajar'))
+print(encounter.test_encounter('colina', 'easy', 'beginner', 'normal'))
+print('')
+print(encounter.get_encounter('geleira', 'easy', 'beginner'))
+print('')
+print(navigation.get_navigation('floresta', True))
+print('')
+print(camping.verifica_acampamento())
+print('')
+print(get_water_result('planicie', 'primavera', 'temperado'))
+
+    
